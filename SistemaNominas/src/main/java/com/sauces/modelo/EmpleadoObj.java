234b7df5ad2f42cdd8fcd537ad3aa7c60b27879d
MBJ-hub <74042647+MBJ-hub@users.noreply.github.com>
2021-05-20 09:25:06 +0200

    Update EmpleadoObj.java

--- SistemaNominas/src/main/java/com/sauces/modelo/EmpleadoObj.java
+++ SistemaNominas/src/main/java/com/sauces/modelo/EmpleadoObj.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.suaces.modelo;
+package com.sauces.modelo;
 
 import com.suaces.sistemanominas.dao.DaoException;
 import com.suaces.sistemanominas.dao.EmpleadoDao;
