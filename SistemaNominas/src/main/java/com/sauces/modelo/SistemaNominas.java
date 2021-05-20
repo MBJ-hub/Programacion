f0cff3cc075a179c18c83a72775bda4af5347960
MBJ-hub <74042647+MBJ-hub@users.noreply.github.com>
2021-05-20 09:31:48 +0200

    Update SistemaNominas.java

--- SistemaNominas/src/main/java/com/sauces/modelo/SistemaNominas.java
+++ SistemaNominas/src/main/java/com/sauces/modelo/SistemaNominas.java
@@ -5,8 +5,8 @@
  */
 package com.sauces.modelo;
 
-import com.suaces.sistemanominas.dao.DaoException;
-import com.suaces.sistemanominas.dao.EmpleadoDao;
+import com.sauces.sistemanominas.dao.DaoException;
+import com.sauces.sistemanominas.dao.EmpleadoDao;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
