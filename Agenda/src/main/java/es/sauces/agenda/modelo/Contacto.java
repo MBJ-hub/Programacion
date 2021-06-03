b13d8ce24c8ded9c18e17eb83a584a0634ab6e66
MBJ-hub <74042647+MBJ-hub@users.noreply.github.com>
2021-06-03 11:22:31 +0200

    Add files via upload

---
+++ Agenda/src/main/java/es/sauces/agenda/modelo/Contacto.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.sauces.agenda.modelo;
+
+/**
+ *
+ * @author daw1
+ */
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Contacto(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+    @Override
+    public int hashCode() {
+        return super.hashCode(); 
+    }
+
+    @Override
+    public String toString() {
+        return nombre+ "," + telefono + "," + email ;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
